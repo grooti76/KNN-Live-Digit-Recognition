3f331f51e81e4c9b5ffbe11616e45eb3aa8f7c93
Vivek Thakur <20509.vivek@gpranchi.org>
2024-04-24 09:58:10 +0530

    commit

--- main/test.py
+++ main/test.py
@@ -1,5 +1,7 @@
-from model.classifier import KNN_DIGIT_CLASSIFIER
+from model.classifier import DIGIT_CLASSIFIER
 
-classifier = KNN_DIGIT_CLASSIFIER('./Dataset/Train.csv')
-x_train, y_train = classifier.process_data()
-classifier.train(x_train,y_train,5)
+# Path: main/test.py
+classifier = DIGIT_CLASSIFIER('data/train.csv')
+x_train,y_train = classifier.process_data()
+classifier.train(x_train,y_train)
+
